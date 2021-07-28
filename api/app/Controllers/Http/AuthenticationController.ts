430fd83f18d749a620f503cc87c8215eeaedb19b
Júlio <julioarmando321@gmail.com>
2021-07-28 15:42:18 -0300

    feat: add authentication

---
+++ api/app/Controllers/Http/AuthenticationController.ts
@@ -0,0 +1,22 @@
+import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import User from 'App/Models/User'
+
+export default class UsersController {
+  public async store({ request, response, auth }: HttpContextContract) {
+    const { email, password } = request.all()
+    try {
+      const { token } = await auth.attempt(email, password)
+      const user = await (await User.findByOrFail("email", email)).serialize()
+      return { ...user, token }
+    } catch (err) {
+      return response.badRequest('Invalid credentials')
+    }
+  }
+
+  public async destroy({ auth }: HttpContextContract) {
+    await auth.use('api').revoke()
+    return {
+      revoked: true
+    }
+  }
+}
--- api/start/routes.ts
+++ api/start/routes.ts
@@ -1,5 +1,8 @@
 import Route from '@ioc:Adonis/Core/Route'
 
+Route.post('login', 'AuthenticationController.store')
+Route.post('logout', 'AuthenticationController.destroy')
+
 Route.resource('user', 'UsersController').apiOnly()
 Route.resource('typetransaction', 'TypeTransactionsController').apiOnly()
 Route.resource('transaction', 'TransactionsController').apiOnly()
